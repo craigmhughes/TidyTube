1e549c1e32202b2cdcd4a6542e0d875fb61fbf52
Craig Hughes <29758432+craigmhughes@users.noreply.github.com>
2018-10-16 01:35:47 +0100

    Fixed bugs which broke video player.
    Added CSS fixes to things previously fixed with JS.

--- background.js
+++ background.js
@@ -1,47 +1,45 @@
-document.getElementById("tt-main").style.margin = "0";
+try {
 
-let enabler = document.getElementById("enabler");
-let loadToggle = document.getElementById("loadToggle");
-let darkToggle = document.getElementById("darkToggle");
+  let enabler = document.getElementById("enabler");
 
-document.getElementById("author").addEventListener("click", function(){
-    let newURL = "https://github.com/craigmhughes/";
-    chrome.tabs.create({ url: newURL });
-});
+  // Redirect to GitHub page
+  document.getElementById("author").addEventListener("click", function(){
+      let newURL = "https://github.com/craigmhughes/";
+      chrome.tabs.create({ url: newURL });
+  });
 
+  setInterval(function(){
+      queryStates();
+  }, 5000);
 
-setInterval(function(){
-    queryStates();
-}, 5000);
+  function queryStates(){
+      chrome.tabs.query({}, function(tabs){
+          for (let i=0; i<tabs.length; ++i) {
+              chrome.tabs.sendMessage(tabs[i].id, {action: "getlocal"});
+          }
+      });
+  }
 
-function queryStates(){
-    chrome.tabs.query({}, function(tabs){
-        for (let i=0; i<tabs.length; ++i) {
-            chrome.tabs.sendMessage(tabs[i].id, {action: "getlocal"});
-        }
-    });
-}
+  let inputs = document.getElementsByClassName("switcher");
 
-let inputs = document.getElementsByClassName("switcher");
+  //  Send update to content.js on input click.
+  for(let i = 0; i < inputs.length; i++){
+      inputs[i].parentNode.addEventListener("click", function(){
+          updateStates();
+      });
+  }
 
-for(let i = 0; i < inputs.length; i++){
-    inputs[i].parentNode.addEventListener("click", function(){
-        updateStates();
-    });
-}
+  function updateStates(){
+      chrome.tabs.query({}, function(tabs){
+          for (let i=0; i<tabs.length; ++i) {
+              chrome.tabs.sendMessage(tabs[i].id, {action: [enabler.checked.toString()]});
+          }
+      });
+  }
 
-function updateStates(){
-    chrome.tabs.query({}, function(tabs){
-        for (let i=0; i<tabs.length; ++i) {
-            chrome.tabs.sendMessage(tabs[i].id, {action: [enabler.checked.toString(),loadToggle.checked.toString(),darkToggle.checked.toString()]});
-        }
-    });
-}
+  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+      enabler.checked = msg[0] === "ttenabledtrue";
+  });
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    enabler.checked = msg[0] === "ttenabledtrue";
-    loadToggle.checked = msg[1] === "loadenabledtrue";
-    darkToggle.checked = msg[2] === "darkmodetrue";
-});
-
-queryStates();
+  queryStates();
+} catch (TypeError){}
--- content.js
+++ content.js
@@ -1,8 +1,10 @@
+
+var content = document.getElementsByClassName("ytd-page-manager");
+
+// Default JSON data.
 let ttJson = {
     "enabled" : "true",
     "refcount" : "false",
-    "showLoader" : "true",
-    "darkmode" : "false"
 };
 
 
@@ -16,143 +18,89 @@ if(window.location.href === "https://www.youtube.com/feed/trending"){
     window.location.replace("https://www.youtube.com");
 }
 
+// Create CSS file addition.
+var cssAdd = document.createElement("style");
+
+/**
+  * Check if page is ready. Then run TidyTube.
+  */
 function ready() {
-    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-        runTidyTube();
+  ttJson = JSON.parse(localStorage.getItem("TidyTube"));
 
-    } else {
-        document.addEventListener('DOMContentLoaded', runTidyTube);
+  if(ttJson.enabled === "true") {
+    document.getElementsByTagName("body")[0].appendChild(cssAdd);
+    cssAdd.innerHTML += `#related{display:none;}
+    .html5-main-video {left:0!important; top:0!important;}
+    .ytd-shelf-renderer {transition-duration: 1s; opacity: 0; pointer-events: none;}
+    a[href="/feed/trending"]{display:none!important}
+    #contents.ytd-section-list-renderer > *.ytd-section-list-renderer:not(:last-child):not(ytd-page-introduction-renderer) {border-bottom: none!important;}
+    `;
+
+    if(window.location.href > 24){
+      cssAdd.innerHTML += `
+      ytd-comments#comments{display:none;}
+      .ytd-watch-next-secondary-results-renderer {display: none;}`;
+    } else{
+      cssAdd.innerHTML += `
+      .ytd-watch-flexy#primary {position: absolute;width: 90%; transition-duration: 1s;}
+      video.html5-main-video {width: -webkit-fill-available!important;height: -webkit-fill-available!important;}
+      .ytp-chrome-bottom {width: 97%!important;}`;
     }
+
+  }
+
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+      runTidyTube();
+  } else {
+      document.addEventListener('DOMContentLoaded', runTidyTube());
+  }
 }
 
+/**
+  * Initial function to start and repeat interval inside.
+  */
 function runTidyTube() {
-    // Allow one second wait for loading DOM. this seems to fix async.
-    ttJson = JSON.parse(localStorage.getItem("TidyTube"));
+  // Allow one second wait for loading DOM. this seems to fix async.
+  setTimeout(function(){
+      pageEdit();
+      testURL();
 
-    setTimeout(function(){
-        ttSetup();
-        setInterval(function () {
-            mainPageEdit();
-            testURL();
-        }, 100);
-    }, 1000);
+      setInterval(function () {
+          pageEdit();
+          testURL();
+      }, 3000);
+  }, 500);
 }
 
-let titleAlert;
-
+/**
+  * Gets info from toggle controller. (background.js)
+  */
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
     if(msg.action === 'getlocal'){
-        chrome.runtime.sendMessage(["ttenabled" + ttJson.enabled, "loadenabled" + ttJson.showLoader, "darkmode" + ttJson.darkmode]);
+        chrome.runtime.sendMessage(["ttenabled" + ttJson.enabled]);
     } else {
         ttJson.enabled = msg.action[0];
-        ttJson.showLoader = msg.action[1];;
-        ttJson.darkmode = msg.action[2];
         storageChange();
     }
 });
 
-
-
-// Sets pre-requisites for functions.
-function ttSetup(){
-    if(ttJson.showLoader === "true" && ttJson.enabled === "true") {
-
-        // Add alert
-        let alertBox = document.createElement('div');
-        alertBox.className = "tt-alert-box tt-title-alert";
-        alertBox.id = "tt-alert";
-        alertBox.innerHTML = `<p>Nothing loading?</p>`;
-
-        if(document.getElementById('primary') !== null) {
-            document.getElementById('masthead').appendChild(alertBox);
-            document.getElementById('primary').style.marginTop = window.location.href.length <= 24 ? "50px" : "0px";
-
-
-            titleAlert = document.getElementById('tt-alert');
-
-            if(ttJson.darkmode === "true")
-                titleAlert.className += " dark";
-
-            titleAlert.addEventListener("mouseover", function(){
-                this.className += " hovered-title";
-                this.childNodes[0].innerText = "Click to Reload";
-            });
-
-            titleAlert.addEventListener("mouseleave", function(){
-                this.className = ttJson.darkmode === "true" ? "tt-alert-box tt-title-alert dark" : "tt-alert-box tt-title-alert";
-                this.childNodes[0].innerText = "Nothing loading?";
-            });
-
-            titleAlert.addEventListener("click", function(){
-                location.reload();
-            });
-        }
-
-    }
-}
-
+/**
+  * Function deletes unneeded related videos from end of current clip.
+  * Also checks if user has changed toggle and reloads page if so.
+  */
 function testURL() {
 
     if(ttJson.enabled === "true"){
-        // Checks if url is that of the home page.
-        if(ttJson.showLoader === "true" && document.getElementById('primary') !== null) {
-            titleAlert.style.visibility = window.location.href.length > 24 ? "hidden" : "visible";
-        }
-
-        try{
-            // Turn off Trending page link.
-            let endpoints = document.getElementsByClassName('yt-simple-endpoint');
-            for (let i = 0; i < endpoints.length; i++) {
-                if (endpoints[i].title.toLowerCase() === "trending") {
-                    endpoints[i].parentNode.style.display = "none";
-                }
-            }
-
-            // Turn off "More from YouTube"
-            let guideRenders = document.getElementsByClassName("ytd-guide-renderer");
-            for (let i = 0; i < guideRenders.length; i++) {
-                if (guideRenders[i].childNodes[3].childNodes[1] !== undefined &&
-                    guideRenders[i].childNodes[3].childNodes[1].innerText.toLowerCase() === "more from youtube") {
-                    guideRenders[i].style.display = "none";
-                }
-            }
-        } catch {
-            // Empty try catch to silence error of trying to affect elements that do not need affecting.
-        }
 
         let url = window.location.href;
         let patt = new RegExp("list");
 
         if(url.length > 24) {
-            if(document.getElementById("top") !== null)
-                document.getElementById("top").style.maxWidth = "var(--flex854-mode-full-width)";
-
-            if(document.getElementById("primary") !== null)
-                document.getElementById("primary").style.minWidth = "100%";
-
-            if (patt.test(url)) {
-                if(document.getElementById("primary") !== null)
-                    document.getElementById("primary").style.minWidth = "";
-            } else {
-                patt = new RegExp("watching now");
-
-                if(document.getElementById("info-text") !== null) {
-                    if (patt.test(document.getElementById("info-text").innerText.toLowerCase())) {
-                        if(document.getElementById("primary") !== null)
-                            document.getElementById("primary").style.minWidth = "";
-                    }
-                }
-            }
-
-            if (document.getElementById("related") !== null)
-                document.getElementById("related").style.display = "none";
-
-
             // Switch off unrelated videos after current play has ended. Checks for keywords matching the title.
             // SUPER HACKY! -- Cannot find closest title class for some reason. childNodes must be used probably because you suck at this.
-
-            let titleEl = document.getElementById("info-contents") === null ? undefined : document.getElementById("info-contents").childNodes[0].childNodes[0].childNodes[2];
-            if(titleEl !== undefined) {
+            try{
+              let titleEl = document.getElementById("info-contents") === null ? undefined : document.getElementById("info-contents").childNodes[0].childNodes[0].childNodes[2];
+              if(titleEl !== undefined) {
                 let titleKeywords = titleEl.innerText.replace(/\(\)/g, " ").toLowerCase().split(/\s+/);
 
                 // Ignore keywords such as common connectives or hyphen.
@@ -186,6 +134,9 @@ function testURL() {
                     // Hides video based on related-ness <-- not a word.
                     endVids[i].closest(".ytp-suggestion-set").style.display = hasKeyword ? "block" : "none";
                 }
+              }
+            } catch (TypeError) {
+              // Missing element to parse.
             }
         }
     }
@@ -198,35 +149,63 @@ function testURL() {
         testURL();
     }
 
-    mainPageEdit();
+    pageEdit();
 }
 
-// Used for if user presses go back in history or forward.
+/**
+  * Used for if user presses go back in history or forward.
+  */
 window.onpopstate = function(){
     testURL();
-    mainPageEdit();
+    pageEdit();
 };
 
+/**
+  * Needed as YouTube doesn't reload any scripts on page change.
+  * Page change != always onpopstate.
+  */
 document.getElementsByTagName("body")[0].addEventListener("click", function(){
     testURL();
-    mainPageEdit();
+    pageEdit();
 });
 
 
-// Switch Toggle States
+/**
+  * Switch Toggle States. Store JSON then restore.
+  */
 function storageChange() {
     localStorage.setItem("TidyTube", JSON.stringify(ttJson));
     location.reload();
 }
 
-function mainPageEdit(){
+/**
+  * Function changes/deletes elements on page based on recommendations.
+  */
+function pageEdit(){
 
     if(ttJson.enabled === "true") {
         if(window.location.href.length <= 24) {
             let patt = new RegExp("recommended");
 
             let sectionSubtitles = document.getElementsByTagName("yt-formatted-string");
+            let listTitles = document.getElementsByTagName("span");
 
+
+            // Check and possibly delete elements containing ignored TITLE string.
+            for(let i = 0; i < listTitles.length; i++){
+              if(listTitles[i].getAttribute("id") === "title"){
+                let ignoredTitles = ["recommended","trending","live gaming for you","live recommendations", "gaming for you"];
+
+                for(let j = 0; j < ignoredTitles.length; j++) {
+                    if (listTitles[i].innerText.toLowerCase().includes(ignoredTitles[j])) {
+                        listTitles[i].closest("#dismissable").style.display = "none";
+                        break;
+                    }
+                }
+              }
+            }
+
+            // Check and possibly delete elements containing ignored SUBTITLE string.
             for(let i = 0; i < sectionSubtitles.length; i++){
                 if( sectionSubtitles[i].id === "title-annotation") {
 
@@ -240,33 +219,93 @@ function mainPageEdit(){
                     for (let j = 0; j < strArr.length; j++) {
                         if (currentTitle === "by " + strArr[j] ||
                             currentTitle === "by " + strArr[j] + " - topic" ||
-                            currentTitle === "by youtube " + strArr[j]) {
+                            currentTitle === "by youtube " + strArr[j] ||
+                            currentTitle === "trending") {
 
-                            sectionSubtitles[i].closest("#dismissable").style.display = "none";
+                            sectionSubtitles[i].closest("#dismissable").parentNode.style.display = "none";
                         }
                     }
                 }
             }
 
-            // Check and possibly delete elements containing ignored title string.
+            // Continue page interaction -- provides more content if page is
+            // being interacted with.
+            scrollBar();
 
-            let sectionTitles = document.getElementsByTagName("ytd-shelf-renderer");
+        } else {
 
-            for(let i = 0; i < sectionTitles.length; i++){
-                let currentTitle = sectionTitles[i].childNodes[1].childNodes[1].childNodes[1].childNodes[1].innerText.toLowerCase();
-                let ignoredTitles = ["recommended","trending","live gaming for you","live recommendations"];
+          showContent();
 
-                for(let j = 0; j < ignoredTitles.length; j++) {
-                    if (currentTitle.substr(0, ignoredTitles[j].length) === ignoredTitles[j]) {
+          try {
 
-                        sectionTitles[i].style.display = "none";
-                        break;
-                    }
-                }
+            let cssAdd2 = document.createElement("style");
+
+            if( (document.getElementsByTagName("yt-view-count-renderer")[0].innerText.includes("watching now")
+                && window.location.href.includes("watch")) || window.location.href.includes("list")){
+
+                  cssAdd2.innerHTML = ".ytd-watch-flexy#primary {position: static;}";
+                  document.getElementsByTagName("body")[0].appendChild(cssAdd2);
+            } else {
+
+              cssAdd2.innerHTML = ".ytd-watch-flexy#primary {position: absolute;width: 90%;}";
+              document.getElementsByTagName("body")[0].appendChild(cssAdd2);
             }
+          }
+          catch (TypeError){}
         }
+
+    } else {
+      showContent();
     }
+
+
+    showVideoLists();
+
 }
+
+/**
+  * Disables initial style per video list.
+  */
+let showVideoLists = () => {
+  let videoLists = document.getElementsByClassName("ytd-shelf-renderer");
+
+  // Fade in effect.
+  for(let i = 0; i < videoLists.length; i++){
+    videoLists[i].style.opacity = "1";
+    videoLists[i].style.pointerEvents = "all";
+  }
+};
+
+/**
+  * Displays main content area.
+  */
+let showContent = () => {
+  for (let i = 0; i < document.getElementsByTagName("ytd-browse").length; i++){
+    document.getElementsByTagName("ytd-browse")[i].style.opacity = "1";
+  }
+}
+
+var scrollInteraction = true;
+
+/**
+  * Affects side scroll to create effect of user engagement.
+  */
+function scrollBar(){
+  let guides = document.getElementsByClassName("ytd-app");
+
+  for(let i = 0; i < guides.length; i++){
+    if(guides[i].getAttribute("id") === "guide-inner-content"){
+      guides[i].scrollTop += scrollInteraction ? 1 : -1;
+      scrollInteraction = !scrollInteraction;
+    }
+  }
+}
+
 
 // Run TidyTube
 ready();
+
+setTimeout(()=>{
+  scrollBar();
+  showContent();
+}, 5000);
